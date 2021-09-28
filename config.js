f4fd6d2a1f186315f406aa1e66a5faa3a81c9394
Seamus 323 <91449071+seamus1826@users.noreply.github.com>
2021-09-28 16:34:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ var config = {
      */
     imgs: {
         //"全世界",
-        // "Eng Tse": "./imgs/birthday.jpg",
+        // "Eng Tse",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
